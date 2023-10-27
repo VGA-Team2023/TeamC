95e03ac751d08116fca50e835c372360d01baf8f
UmenoKoudai <umenokoudai0805@gmail.com>
2023-10-28 00:09:48 +0900

    [Add]Waveシステム出来た(あとは他のスクリプトと連携)

--- Assets/Programer/Script/MeleeEnemy.cs
+++ Assets/Programer/Script/MeleeEnemy.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Ghost : EnemyBase
+public class MeleeEnemy : EnemyBase
 {
     [SerializeField, Tooltip("どれくらいベース位置に近づいたら次の目標に向かうか")]
     float _distance;
@@ -14,45 +12,53 @@ public class Ghost : EnemyBase
     GameObject _destination;
     [SerializeField]
     GameObject _base;
+
     Vector3 _basePos;
     Rigidbody _rb;
     Vector3 _dir;
     bool _isArrived = true;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _basePos = transform.position;
-        Instantiate(_base, _basePos, Quaternion.identity);
-        _dir = new Vector3
-                (Mathf.Sin(Random.Range(0, 361)) * _moveRange,
-                _basePos.y,
-                Mathf.Cos(Random.Range(0, 361) * _moveRange));
-        Instantiate(_destination, new Vector3(_dir.x, _basePos.y, _dir.z), Quaternion.identity);
+        //Instantiate(_base, _basePos, Quaternion.identity);
+        _dir = GetMovePoint();
+        transform.forward = (_dir - transform.position).normalized;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float baseDis = Vector3.Distance(transform.position, _basePos);
         float destinationDis = Vector3.Distance(transform.position, _dir);
-        
         if (baseDis < _distance && !_isArrived)
         {
-            float random = Random.Range(0, 361);
-            _dir = new Vector3
-                (Mathf.Sin(random) * _moveRange,
-                _basePos.y,
-                Mathf.Cos(random) * _moveRange);
-            Instantiate(_destination, new Vector3(_dir.x, _basePos.y, _dir.z), Quaternion.identity);
+            _dir = GetMovePoint();
+            transform.forward = (_dir - transform.position).normalized;
             _isArrived = true;
         }
         else if(destinationDis < _distance && _isArrived)
         {
-            _dir = (_basePos - transform.position).normalized;
+            _dir = _basePos - transform.position;
+            transform.forward = _dir.normalized;
             _isArrived = false;
         }
-        transform.forward = _dir;
         _rb.velocity = transform.forward * base.Speed;
         transform.position = new Vector3(transform.position.x, _basePos.y, transform.position.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _dir = _basePos - transform.position;
+        transform.forward = _dir.normalized;
+        _isArrived = false;
+    }
+
+    Vector3 GetMovePoint()
+    {
+        float random = Random.Range(0, 361);
+        var dir =  new Vector3(Mathf.Sin(random) * _moveRange + transform.position.x, _basePos.y, Mathf.Cos(random) * _moveRange + transform.position.z);
+        //Instantiate(_destination, new Vector3(dir.x, _basePos.y, dir.z), Quaternion.identity);
+        return dir;
     }
 }
