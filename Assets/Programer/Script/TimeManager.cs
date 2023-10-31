efcd54c8bdff5fb87a0fcdcfb5bd4673f51560af
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-10-31 18:19:24 +0900

    [fix/update]タイムマネージャー更新

--- Assets/Programer/Script/TimeManager.cs
+++ Assets/Programer/Script/TimeManager.cs
@@ -2,15 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 /// <summary>ゲームの経過時間を操作するClass</summary>
-public class TimeManager : MonoBehaviour
+public class TimeManager : ISlow,IPause
 {
+    float _currentTimeSpeedRate = 1;
+    /// <summary>ゲームのプレイ時間</summary>
+    [SerializeField] float _gamePlayTime = 60;
     /// <summary>ゲーム中の経過時間</summary>
-    float _elapsedTime = 0;
-    public float ElapsedTime => _elapsedTime;
-
-    void Update()
+    float _gamePlayElapsedTime = 0;
+    public float GamePlayElapsedTime => _gamePlayElapsedTime;
+    public TimeManager()
     {
-        _elapsedTime += Time.deltaTime;
+        TimerReset();
+    }
+
+    public void Start()
+    {
+        GameManager.Instance.PauseManager.Add(this);
+        GameManager.Instance.SlowManager.Add(this);
+    }
+    /// <summary>主にタイムの時間を減らす処理を行う関数</summary>
+    public void Update()
+    {
+        _gamePlayElapsedTime -= Time.deltaTime * _currentTimeSpeedRate;
+    }
+    /// <summary>ゲーム時間のリセット</summary>
+    public void TimerReset()
+    {
+        _gamePlayElapsedTime = _gamePlayTime;
+    }
+
+    void IPause.Pause()
+    {
+        _currentTimeSpeedRate = 0;
+    }
+
+    void IPause.Resume()
+    {
+        _currentTimeSpeedRate = 1;
+    }
+
+    void ISlow.OffSlow() 
+    {
+        _currentTimeSpeedRate = 1;
+    }
+
+    void ISlow.OnSlow(float slowSpeedRate)
+    {
+        _currentTimeSpeedRate = slowSpeedRate;
     }
 }
