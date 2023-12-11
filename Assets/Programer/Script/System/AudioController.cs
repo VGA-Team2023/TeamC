10c3ec02b7503e5a8503037053e1ee50849d2f09
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-12-12 08:52:34 +0900

    [fix]修正

--- Assets/Programer/Script/System/AudioController.cs
+++ Assets/Programer/Script/System/AudioController.cs
@@ -1,27 +1,35 @@
 ﻿using UnityEngine;
 using CriWare;
+using UniRx;
+using System;
 
 /// <summary>CueNameのデータと音をの再生する機能を保持・管理するクラス</summary>
 public class AudioController : MonoBehaviour
 {
     /// <summary>シングルトン化</summary>
     static AudioController _instance;
-
+    CriAudioManager _criAudioManager;
+    [Header("音量調整")]
+    [SerializeField, Tooltip("BGM"), Range(0,1)] float _bgmVolume = 1; 
+    [SerializeField, Tooltip("SE"), Range(0, 1)] float _seVolume = 1; 
+    [SerializeField, Tooltip("Voice"), Range(0, 1)] float _voiceVolume = 1;
+    [SerializeField, Tooltip("ME"), Range(0, 1)] float _meVolume = 1;
+    [Space]
+    [Space]
+    [Header("CueSheetName・CueNameの設定(プログラマー用)")]
     [SerializeField,Tooltip("キューシートの名前")] string _cueSheetName = "Sound";
     [SerializeField,Tooltip("SEのCueNameのデータ")] SEAudioControlle _se;
     [SerializeField,Tooltip("BGMのCueNameのデータ")] BGMAudioControlle _bgm;
     [SerializeField,Tooltip("VoiceのCueNameのデータ")] VoiceAudioControlle _voice;
-
     /// <summary>シーン上にあるリスナーコンポーネント</summary>
     CriAtomListener _listener = null;
-
-    VolumeChange volumeChange = new();
-    
     public SEAudioControlle SE => _se;
     public BGMAudioControlle BGM => _bgm;
     public VoiceAudioControlle Voice => _voice;
-    /// <summary>ボリューム設定</summary>
-    public VolumeChange VolumeChange => volumeChange;
+
+    public float BgmVolume => _bgmVolume;
+    public float VoiceVolume => _voiceVolume;
+    public float SeVolume => _seVolume;
     public static AudioController Instance
     {
         get
@@ -29,8 +37,10 @@ public class AudioController : MonoBehaviour
             if (!_instance)
             {
                 _instance = FindObjectOfType<AudioController>();
+                _instance._criAudioManager = CriAudioManager.Instance;
                 _instance.CueSheetNameSet();
                 _instance.SetListener();
+
                 if (!_instance)
                 {
                     Debug.LogError("Scene内に" + typeof(AudioController).Name + "をアタッチしているGameObjectがありません");
@@ -44,6 +54,7 @@ public class AudioController : MonoBehaviour
         if (_instance == null)
         {
             _instance = this;
+            _instance._criAudioManager = CriAudioManager.Instance;
             CueSheetNameSet();
             SetListener();
             DontDestroyOnLoad(this);
@@ -54,10 +65,18 @@ public class AudioController : MonoBehaviour
         }
         else
         {
+            _instance._criAudioManager = CriAudioManager.Instance;
             CueSheetNameSet();
             SetListener();
             Destroy(this);
         }
+    }
+
+    public void Update()
+    {
+        _criAudioManager.BGM.Volume.Value = _bgmVolume;
+        _criAudioManager.SE.Volume.Value = _seVolume;
+        _criAudioManager.Voice.Volume.Value = _voiceVolume;
     }
     public void CueSheetNameSet()
     {
@@ -69,34 +88,32 @@ public class AudioController : MonoBehaviour
     public void SetListener()
     {
         _instance._listener = Camera.main.GetComponent<CriAtomListener>();
-        CriAudioManager.Instance.BGM.SetListenerAll(_instance._listener);
-        CriAudioManager.Instance.SE.SetListenerAll(_instance._listener);
-        CriAudioManager.Instance.Voice.SetListenerAll(_instance._listener);
+        _instance._criAudioManager.BGM.SetListenerAll(_instance._listener);
+        _instance._criAudioManager.SE.SetListenerAll(_instance._listener);
+        _instance._criAudioManager.Voice.SetListenerAll(_instance._listener);
     }
-}
-
-public class VolumeChange
-{
-    public enum Type
-    {
-        Master,
-        BGM,
-        SE,
-        Voice,
-    }
-    public void OnVolumeChange(float value, Type type)
+    
+    public void OnVolumeChange(float value, VolumeChangeType type)
     {
         switch (type)
         {
-            case Type.Master:
-                CriAudioManager.Instance.MasterVolume.Value = value; break;
-            case Type.BGM:
-                CriAudioManager.Instance.BGM.Volume.Value = value; break;
-            case Type.SE:
-                CriAudioManager.Instance.SE.Volume.Value = value; break;
-            case Type.Voice:
-                CriAudioManager.Instance.Voice.Volume.Value = value; break;
+            //case VolumeChangeType.Master:
+            //    CriAudioManager.Instance.MasterVolume.Value = value; break;
+            case VolumeChangeType.BGM:
+                _instance._bgmVolume = value; break;
+            case VolumeChangeType.SE:
+                _instance._seVolume = value; break;
+            case VolumeChangeType.Voice:
+                _instance._voiceVolume = value; break;
         }
-    }
+    }  
+}
+
+public enum VolumeChangeType
+{
+    //Master,
+    BGM,
+    SE,
+    Voice,
 }
 
