c7501627b37a047df09a4422443d864506aff28b
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-12-18 13:55:28 +0900

    [update]Enum追加主にBossのSE

--- Assets/Programer/Script/System/AudioEnum.cs
+++ Assets/Programer/Script/System/AudioEnum.cs
@@ -45,6 +45,10 @@ public enum SEState
     PlayerDodgeGrass,
     /// <summary>Player遠距離敵からのダメージ</summary>
     PlayerLongAttackEnemyDamage,
+    /// <summary>Playerボス敵からのダメージ(ボスが氷の場合)</summary>
+    PlayerBossEnemyHitIce,
+    /// <summary>Playerボス敵からのダメージ(ボスが草の場合)</summary>
+    PlayerBossEnemyHitGrass,
 
 
     /// <summary>Player通常攻撃が当たった時のHit音(氷パターン1)</summary>
@@ -65,6 +69,22 @@ public enum SEState
     EnemyLongAttackShoot,
     /// <summary>遠敵の攻撃のトレイル音</summary>
     EnemyLongAttackTrail,
+    /// <summary>ボス敵の通常攻撃(氷)</summary>
+    EnemyBossShootIce,
+    /// <summary>ボス敵の通常攻撃(草)</summary>
+    EnemyBossShootGrass,
+    /// <summary>ボス敵の強攻撃チャージ中(氷)</summary>
+    EnemyBossChargeIce,
+    /// <summary>ボス敵の強攻撃チャージ中(草)</summary>
+    EnemyBossChargeGrass,
+    /// <summary>ボス敵のトレイル音(氷)</summary>
+    EnemyBossTrailIce,
+    /// <summary>ボス敵のトレイル音(草)</summary>
+    EnemyBossTrailGrass,
+    /// <summary>ボス敵の強攻撃(氷)</summary>
+    EnemyBossFinishIce,
+    /// <summary>ボス敵の強攻撃(草)</summary>
+    EnemyBossFinishGrass,
     /// <summary>Playerの通常攻撃時のダメージ音</summary>
     EnemyNormalDamage,
     /// <summary>Playerのトドメ攻撃時のダメージ音</summary>
@@ -75,8 +95,7 @@ public enum SEState
     EnemyIn,
     /// <summary>退場音</summary>
     EnemyOut,
-
-
+    
     /// <summary>システムの決定音</summary>
     SystemApply,
     /// <summary>システムのキャンセル音</summary>
@@ -97,52 +116,57 @@ public enum SEState
 
 public enum VoiceState
 {
-    /// <summary>通常攻撃(はっ！、ふっ！、やっ！、これで！)</summary>
+    /// <summary>通常攻撃(はっ！)(ふっ！)(やっ！)(これで！)</summary>
     PlayerAttack,
-    /// <summary>氷魔法陣展開(グレイシオ！、まだまだ！、次・・・！)</summary>
+    /// <summary>氷魔法陣展開(グレイシオ！)(まだまだ！)(次・・・！)</summary>
     PlayerCastingIce,
-    /// <summary>草魔法陣展開(ロゼエラ！、、まだまだ！、次・・・！)</summary>
+    /// <summary>草魔法陣展開(ロゼエラ！)(まだまだ！)(次・・・！)</summary>
     PlayerCastingGrass,
-    /// <summary>氷魔法チャージ中(冷気よ・・・！、氷よ・・・！、凍れ・・・！)</summary>
+    /// <summary>氷魔法チャージ中(冷気よ・・・！)(氷よ・・・！)(凍れ・・・！)</summary>
     PlayerChargeIce,
-    /// <summary>草魔法チャージ中(薔薇よ・・・！、緑よ・・・！、命よ・・・！)</summary>
+    /// <summary>草魔法チャージ中(薔薇よ・・・！)(緑よ・・・！)(命よ・・・！)</summary>
     PlayerChargeGrass,
-    /// <summary>氷魔法必殺攻撃(熱を奪う！、トドメ！、これで終わり！、コアを！)</summary>
+    /// <summary>氷魔法必殺攻撃(熱を奪う！)(トドメ！)(これで終わり！)(コアを！)</summary>
     PlayerFinishIce,
-    /// <summary>草魔法必殺攻撃(芽吹け・・・！、トドメ！、これで終わり！、コアを！)</summary>
+    /// <summary>草魔法必殺攻撃(芽吹け・・・！)(トドメ！)(これで終わり！)(コアを！)</summary>
     PlayerFinishGrass,
-    /// <summary>回避(せっ！、っと！、っしょ！)</summary>
+    /// <summary>属性変更草から氷に変更するとき(氷で！)(氷魔法！)(こっちを！)</summary>
+    PlayerAttributeChangeIce,
+    /// <summary>属性変更氷から草に変更するとき(草で！)(草魔法！)(こっちを！)</summary>
+    PlayerAttributeChangeGrass,
+    /// <summary>回避(せっ！)(っと！)(っしょ！)</summary>
     PlayerDodge,
-    /// <summary>ダメージ(うっ！、痛った！、ぐっ！)</summary>
+    /// <summary>ダメージ(うっ！)(痛った！)(ぐっ！)</summary>
     PlayerDamage,
-    /// <summary>HP0(こんな・・・ところで・・・、まだ・・・戦える・・・、悔しい！)</summary>
+    /// <summary>HP0(こんな・・・ところで・・・)(まだ・・・戦える・・・)(悔しい！)</summary>
     PlayerDown,
 
-    /// <summary>索敵(オオオオォォ・・・・)</summary>
-    EnemySaerch,
-    /// <summary>近距離攻撃の敵Player発見(キッ！、ヒヒヒ！)</summary>
+    /// <summary>近距離攻撃の敵索敵(オオオオォォ・・・・)</summary>
+    EnemyShortSaerch,
+    /// <summary>遠距離攻撃の敵索敵(オオオオォォ・・・・)</summary>
+    EnemyLongSaerch,
+    /// <summary>近距離攻撃の敵Player発見(キッ！)(ヒヒヒ！)</summary>
     EnemyShortDiscov,
-    /// <summary>遠距離攻撃の敵Player発見(キッ！、ヒヒヒ！)</summary>
+    /// <summary>遠距離攻撃の敵Player発見(キッ！)(ヒヒヒ！)</summary>
     EnemyLongDiscov,
-    /// <summary>近距離攻撃の攻撃(キヒャ！、ケケケケ！)</summary>
+    /// <summary>近距離攻撃の攻撃(キヒャ！)(ケケケケ！)</summary>
     EnemyShortAttack,
-    /// <summary>遠距離攻撃の攻撃(キヒャ！、ケケケケ！)</summary>
+    /// <summary>遠距離攻撃の攻撃(キヒャ！)(ケケケケ！)</summary>
     EnemyLongAttack,
-    /// <summary>近距離攻撃のダメージ(キーッ!?、ギ!?)</summary>
+    /// <summary>近距離攻撃のダメージ(キーッ!?)(ギ!?)</summary>
     EnemyShortDamage,
-    /// <summary>遠距離攻撃のダメージ(キーッ!?、ギ!?)</summary>
+    /// <summary>遠距離攻撃のダメージ(キーッ!?)(ギ!?)</summary>
     EnemyLongDamage,
-    /// <summary>近距離攻撃の死(ギーッ!、ヒヒャーッ!)</summary>
+    /// <summary>近距離攻撃の死(ギーッ!)(ヒヒャーッ!)</summary>
     EnemyShortDeath,
-    /// <summary>遠距離攻撃の死(ギーッ!、ヒヒャーッ!)</summary>
+    /// <summary>遠距離攻撃の死(ギーッ!)(ヒヒャーッ!)</summary>
     EnemyLongDeath,
 
-    /// <summary>チュートリアル開始時(これより、アルシオネ魔法学園の、進級試験を始めます)</summary>
+    /// <summary>チュートリアル開始時
+    /// (これより、アルシオネ魔法学園の、進級試験を始めます)
+    /// (試験官は私、ミリアが担当するわね)
+    /// (最初はノエリアさんの出番ね、頑張ってちょうだい)</summary>
     InstructorTutorialStartPattern1,
-    /// <summary>チュートリアル開始時(試験官は私、ミリアが担当するわね)</summary>
-    InstructorTutorialStartPattern2,
-    /// <summary>チュートリアル開始時(最初はノエリアさんの出番ね、頑張ってちょうだい)</summary>
-    InstructorTutorialStartPattern3,
     /// <summary>チュートリアル確認(貴方はとってもイイ感じなんだけど、不安だったらおさらいしておく?)</summary>
     InstructorTutorialCheck,
     /// <summary>チュートリアル確認後(分かったわ。それじゃあまずは)</summary>
@@ -183,16 +207,14 @@ public enum VoiceState
     InstructorTutorialOption,
     /// <summary>オプションチュートリアル完了(音が大きすぎる～って時は利用して頂戴ね！)</summary>
     InstructorTutorialOptionOK,
-    /// <summary>ゲームクリア(は～い、試験終了ようふふっ、ノエリアさん、お疲れ様〜)</summary>
-    InstructorGameClearPattern1,
-    /// <summary>ゲームクリア(そこまで！よく頑張ったわね〜後で先生から合否を送るわね〜)</summary>
-    InstructorGameClearPattern2,
-    /// <summary>ゲームクリア(終了よ！今すぐ結果を報告したいけど、ちょっと待っててね〜)</summary>
-    InstructorGameClearPattern3,
-    /// <summary>回復(あらあら、調子が悪かったのかしら？まだまだ頑張れるわ！ファイト〜！)</summary>
-    InstructorGameHealPattern1,
-    /// <summary>回復(大丈夫？先生の特別サ-ビスよ〜)</summary>
-    InstructorGameHealPattern2,
-    /// <summary>回復(しっかり！貴方の実力はこんなものじゃないわ～！)</summary>
-    InstructorGameHealPattern3,
+    /// <summary>ゲームクリア
+    /// (は～い、試験終了ようふふっ、ノエリアさん、お疲れ様〜)
+    /// (そこまで！よく頑張ったわね〜後で先生から合否を送るわね〜)
+    /// (終了よ！今すぐ結果を報告したいけど、ちょっと待っててね〜)</summary>
+    InstructorGameClear,
+    /// <summary>回復
+    /// (あらあら、調子が悪かったのかしら？まだまだ頑張れるわ！ファイト〜！)
+    /// (大丈夫？先生の特別サ-ビスよ〜)
+    /// (しっかり！貴方の実力はこんなものじゃないわ～！)</summary>
+    InstructorGameHeal,
 }
