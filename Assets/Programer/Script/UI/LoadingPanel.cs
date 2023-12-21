d63a2e7b355b1f1a81a284d720de2c1a3e467fea
OoyamaShun130724 <shun20010724@icloud.com>
2023-12-21 11:41:27 +0900

    Loadingを修正及びUI画像適用

--- Assets/Programer/Script/UI/LoadingPanel.cs
+++ Assets/Programer/Script/UI/LoadingPanel.cs
@@ -2,14 +2,20 @@
 using UnityEngine.UI;
 public class LoadingPanel : MonoBehaviour
 {
-    [SerializeField] Image[] _images;
-    [SerializeField] Image _leftImage;
-    [SerializeField] Image _rightImage;
+    //[SerializeField] Image[] _images;
+    //[SerializeField] Image _leftImage;
+    //[SerializeField] Image _rightImage;
     [SerializeField] GameObject _rotatateObj;
     [SerializeField] Text _tips;
     [SerializeField] string[] _tipsData;
-    private int _leftInd = 0;
-    private int _rightInd = 0;
+    [SerializeField] TipData[] _tipDatas;
+    [SerializeField] Text _teacherText;
+    [SerializeField] Text _magicText;
+    [SerializeField] Text _academyText;
+    [SerializeField] Text _operationInstructionsText;
+    [SerializeField] Text _mainCharactorText;
+    //private int _leftInd = 0;
+    //private int _rightInd = 0;
     private int _totalRotate = 0;
     private static LoadingPanel _instance = null;
     public static LoadingPanel Instance
@@ -27,18 +33,18 @@ public class LoadingPanel : MonoBehaviour
         _instance = this;
         DontDestroyOnLoad(this);
 
-        _leftInd = Random.Range(0, _images.Length);
+        //_leftInd = Random.Range(0, _images.Length);
 
-        if (_images.Length > 1)
-        {
-            while (_rightInd == _leftInd)
-            {
-                _rightInd = Random.Range(0, _images.Length);
-            }
-        }
+        //if (_images.Length > 1)
+        //{
+        //    while (_rightInd == _leftInd)
+        //    {
+        //        _rightInd = Random.Range(0, _images.Length);
+        //    }
+        //}
 
-        _leftImage.sprite = _images[_leftInd].sprite;
-        _rightImage.sprite = _images[_rightInd].sprite;
+        //_leftImage.sprite = _images[_leftInd].sprite;
+        //_rightImage.sprite = _images[_rightInd].sprite;
 
         PrintTips();
     }
@@ -49,7 +55,19 @@ public class LoadingPanel : MonoBehaviour
     }
     public void PrintTips()
     {
-        int random = Random.Range(0, _tipsData.Length);
-        _tips.text = _tipsData[random];
+        _mainCharactorText.gameObject.SetActive(false);
+        _operationInstructionsText.gameObject.SetActive(false);
+        _academyText.gameObject.SetActive(false);
+        _magicText.gameObject.SetActive(false);
+        _teacherText.gameObject.SetActive(false);
+
+        int random = Random.Range(0, _tipDatas.Length);
+        _tips.text = _tipDatas[random].TipText;
+
+        if(_tipDatas[random].Type==TipData.TipsType.Teacher) _teacherText.gameObject.SetActive(true);
+        else if(_tipDatas[random].Type==TipData.TipsType.MainCharacter) _mainCharactorText.gameObject.SetActive(true);
+        else if(_tipDatas[random].Type==TipData.TipsType.OperationInstructions) _operationInstructionsText.gameObject.SetActive(true);
+        else if(_tipDatas[random].Type==TipData.TipsType.Academy) _academyText.gameObject.SetActive(true);
+        else if(_tipDatas[random].Type==TipData.TipsType.Magic) _magicText.gameObject.SetActive(true);
     }
 }
